2a895e2db4188ea53c6c843e3828cf9ebfd13c8a
Abu Bakar Wase <33964884+abubakarwase@users.noreply.github.com>
2020-10-14 23:21:35 +0500

    resolve the issues

--- controllers/html-parser.js
+++ controllers/html-parser.js
@@ -2,23 +2,20 @@ const asyncHandler = require("../middlewares/async");
 const axios = require("axios");
 
 exports.getHtml = asyncHandler(async (req, res, next) => {
-  console.log(req.query.address);
+  if (!req.query.address)
+    return res.status(500).json({ error: "No websites are provided" });
   let addressToBeHit = req.query.address.map((item) => axios.get(`${item}`));
   axios
     .all(addressToBeHit)
     .then(
-      axios.spread((response1, response2, response3, response4, response5) => {
+      axios.spread((...response) => {
         res.status(200).json({
           success: true,
-          data1: response1.data,
-          data2: response2.data,
-          data3: response3.data,
-          data4: response4.data,
-          data5: response5.data,
+          data: response.map((item) => item.data),
         });
       })
     )
     .catch((error) => {
-      console.log(error);
+      res.status(500).json({ error: error.message });
     });
 });
